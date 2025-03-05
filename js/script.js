d331d4b9c39ace55edd1eb880f52c5d62ff88e8c
FJ9019 <fenelus24@gmail.comgit config --global user.email fenelus24@gmail.comgit config --global user.email fenelus24@gmail.com>
2025-03-05 11:32:58 -0500

    All Page Update

--- js/script.js
+++ js/script.js
@@ -75,6 +75,35 @@
     });
 
 
+    // Partner
+    $(document).ready(function () {
+        var owl = $("#owl-demo1");
+      
+        owl.owlCarousel({
+          items: 4, //10 items above 1000px browser width
+          itemsDesktop: [1000, 3], //5 items between 1000px and 901px
+          itemsDesktopSmall: [900, 2], // 3 items betweem 900px and 601px
+          itemsTablet: [600, 1], //2 items between 600 and 0;
+          itemsMobile: [360, 1] // itemsMobile disabled - inherit from itemsTablet option
+        });
+      
+        // Custom Navigation Events
+        $(".next").click(function () {
+          owl.trigger("owl.next");
+        });
+        $(".prev").click(function () {
+          owl.trigger("owl.prev");
+        });
+        $(".play").click(function () {
+          owl.trigger("owl.play", 1000);
+        });
+        $(".stop").click(function () {
+          owl.trigger("owl.stop");
+        });
+      });
+      
+
+
    // Back to top button
    $(window).scroll(function () {
     if ($(this).scrollTop() > 300) {
