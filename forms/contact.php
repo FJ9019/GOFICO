d81163683548d0690679fef72ddbd96accb21cb7
FJ9019 <fenelus24@gmail.comgit config --global user.email fenelus24@gmail.comgit config --global user.email fenelus24@gmail.com>
2025-04-01 13:05:44 -0400

    Change In Apropos Page

--- forms/contact.php
+++ forms/contact.php
@@ -1,30 +1,45 @@
 <?php
-
-
-  // Receiving email address
-  $receiving_email_address = 'tech@gofico.net';
-
-  if( file_exists($php_email_form = '../lib/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
-
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
-
-
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  if(isset($_POST['phone'])) {
-    $contact->add_message( $_POST['phone'], 'Phone');
-  }
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Collect and sanitize input data
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $subject = htmlspecialchars($_POST['subject']);
+    $message = htmlspecialchars($_POST['message']);
+    
+    // Validate email
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo "Invalid email format";
+        exit;
+    }
+    
+    // Set the recipient email address
+    $to = "tech@gofico.net";
+    
+    // Set email headers
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+    
+    // Compose the email body
+    $email_body = "You have received a new message from the contact form on your website.\n\n";
+    $email_body .= "Name: $name\n";
+    $email_body .= "Email: $email\n";
+    $email_body .= "Subject: $subject\n";
+    $email_body .= "Message:\n$message\n";
+    
+    // Send the email
+    if (mail($to, $subject, $email_body, $headers)) {
+        // Redirect to the contact page with a success message
+        header("Location: ../contactez-nous.html?status=success");
+        exit;
+    } else {
+        // Redirect to the contact page with an error message
+        header("Location: ../contactez-nous.html?status=error");
+        exit;
+    }
+} else {
+    // Redirect to the contact page if the request method is not POST
+    header("Location: ../contactez-nous.html");
+    exit;
+}
 ?>
